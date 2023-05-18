d10bc18a580bf3c0b2be9a20e29ef4a38787dd48
Shuyibe48 <gthrough48@gmail.com>
2023-05-18 17:43:20 +0600

    footer fixed with bg color

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -3,30 +3,30 @@ import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-white">
+    <footer className="bg-[#ffc800] text-black px-4 lg:px-0">
       <div className="container mx-auto py-8">
-        <div className="flex items-center justify-between mb-4">
-          <div className="flex items-center">
+        <div className="lg:flex items-center justify-between mb-4">
+          <div className="flex mb-4 lg:mb-0 items-center">
             <img src="https://www.tailorbrands.com/wp-content/uploads/2020/07/mcdonalds-logo.jpg" alt="MiniMotorMania" className="w-8 h-8 mr-2" />
             <span className="text-xl font-bold">MiniMotorMania</span>
           </div>
           <div className="flex space-x-4">
             <a href="https://www.facebook.com/minimotormania">
-              <FaFacebook className="text-white hover:text-blue-500" />
+              <FaFacebook className="text-black hover:text-white" />
             </a>
             <a href="https://www.twitter.com/minimotormania">
-              <FaTwitter className="text-white hover:text-blue-400" />
+              <FaTwitter className="text-black hover:text-white" />
             </a>
             <a href="https://www.instagram.com/minimotormania">
-              <FaInstagram className="text-white hover:text-pink-500" />
+              <FaInstagram className="text-black hover:text-white" />
             </a>
           </div>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           <div>
             <h4 className="text-lg font-semibold mb-4">Contact</h4>
-            <p className="text-gray-400">
-              Address: 1234 Example Street, City, Country
+            <p className="text-gray-800">
+              Address: 1234 Dhanmondi Street, Dhaka, Bangladesh 
               <br />
               Email: info@minimotormania.com
               <br />
@@ -37,21 +37,21 @@ const Footer = () => {
             <h4 className="text-lg font-semibold mb-4">Follow Us</h4>
             <div className="flex space-x-4">
               <a href="https://www.facebook.com/minimotormania">
-                <FaFacebook className="text-white hover:text-blue-500" />
+                <FaFacebook className="text-black hover:text-white" />
               </a>
               <a href="https://www.twitter.com/minimotormania">
-                <FaTwitter className="text-white hover:text-blue-400" />
+                <FaTwitter className="text-black hover:text-white" />
               </a>
               <a href="https://www.instagram.com/minimotormania">
-                <FaInstagram className="text-white hover:text-pink-500" />
+                <FaInstagram className="text-black hover:text-white" />
               </a>
             </div>
           </div>
         </div>
       </div>
-      <div className="bg-gray-800 py-4">
+      <div className="bg-yellow-400 py-4">
         <div className="container mx-auto text-center">
-          <p className="text-gray-400">
+          <p className="text-white">
             &copy; {new Date().getFullYear()} MiniMotorMania. All rights reserved.
           </p>
         </div>
