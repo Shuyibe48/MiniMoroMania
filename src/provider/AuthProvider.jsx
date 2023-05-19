777354b43da50da1deca3b50f547a8e359ffe4fb
Shuyibe48 <gthrough48@gmail.com>
2023-05-20 04:22:15 +0600

    firebase implement

---
+++ src/provider/AuthProvider.jsx
@@ -0,0 +1,82 @@
+import { createContext, useEffect, useState } from 'react';
+import { GithubAuthProvider, GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import app from '../firebase/firebase.config';
+
+export const AuthContext = createContext(null)
+
+const auth = getAuth(app)
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider()
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [chefs, setChefs] = useState([])
+
+    useEffect(() => {
+        fetch('https://chef-savant-server-shuyibe48.vercel.app/chefinfo')
+        .then(res => res.json())
+        .then(data => setChefs(data))
+    }, [])
+
+
+    const createUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const signInWithGoogle = () => {
+        return signInWithPopup(auth, googleProvider)
+    }
+
+    const signInWithGithub = () => {
+        return signInWithPopup(auth, githubProvider)
+    }
+
+    const signIn = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const updateUser = (currentUser, name, photo) => {
+        return updateProfile(currentUser, { displayName: name, photoURL: photo })
+    }
+
+    const logOut = () => {
+        return signOut(auth)
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser)
+            setLoading(false)
+        })
+
+        // stop observing while unmounting
+        return () => {
+            unsubscribe()
+        }
+    }, [])
+
+
+
+    const userInfo = {
+        user,
+        createUser,
+        signInWithGoogle,
+        signInWithGithub,
+        signIn,
+        updateUser,
+        logOut,
+        loading,
+        chefs
+    }
+
+    return (
+        <AuthContext.Provider value={userInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
